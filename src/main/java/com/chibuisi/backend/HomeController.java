d0ccb6e3fdc726b086392e50e058e4b1138cf79a
chibuisi <amiaka.chibuisi@gmail.com>
2020-02-07 10:35:31 -0600

    added home controller and testing web hooks on github

---
+++ src/main/java/com/chibuisi/backend/HomeController.java
@@ -0,0 +1,16 @@
+package com.chibuisi.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "You have reached the home page";
+    }
+}
